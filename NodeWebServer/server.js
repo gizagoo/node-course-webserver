ffba4a66a3dc311126dc74fb9d96421c45e41913
Frank Murphy <frank.nds@gmail.com>
2017-09-15 17:11:34 +0100

    Setup for Heroku

--- NodeWebServer/server.js
+++ NodeWebServer/server.js
@@ -1,6 +1,7 @@
 ﻿const express = require('express');
 const hbs = require('hbs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -41,6 +42,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
 });
